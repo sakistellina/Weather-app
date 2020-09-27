d3f15724a9f1af9c425fd50d3943599e2548076b
sakistellina <69436794+sakistellina@users.noreply.github.com>
2020-09-27 20:49:24 +0200

    Showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apikey = "8f2a727a8d5b5cadf3134d55a1087a44";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=New York&appid=8f2a727a8d5b5cadf3134d55a1087a44&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
